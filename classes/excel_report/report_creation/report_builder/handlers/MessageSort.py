0cc13b3babb423c096642d7562de075108ce18f0
Yaroslav Kovalev <yaroskov@yandex.ru>
2021-08-25 22:51:15 +0300

    1. Implemented search data into bags results; 2. Optimization in MessageSort.

--- classes/excel_report/report_creation/report_builder/handlers/MessageSort.py
+++ classes/excel_report/report_creation/report_builder/handlers/MessageSort.py
@@ -1,5 +1,6 @@
 from classes.data.Data import Data
 from classes.excel_report.report_creation.report_builder.handlers.LikeFinder import LikeFinder
+import re
 
 
 class MessageSort(Data):
@@ -24,18 +25,10 @@ class MessageSort(Data):
 
                 if curr_message != item["body2"] and not like:
                     curr_message = item["body2"]
-                    cleared_msg = curr_message.replace("\n ", "")
-                    new_block = {
-                        "message": cleared_msg,
-                        "incidentsNumber": 1,
-                        "data": [item]
-                    }
-                    results["errors"].append(new_block)
-                    new_light = {
-                        "message": cleared_msg,
-                        "incidentsNumber": 1
-                    }
-                    results["light"].append(new_light)
+                    cleared_msg = MessageSort.clear_msg(curr_message)
+                    search_data = MessageSort.search_data(cleared_msg)
+                    results["errors"].append(MessageSort.new_block_full(cleared_msg, search_data, item))
+                    results["light"].append(MessageSort.new_block_light(cleared_msg, search_data))
                 else:
                     results["errors"][-1]["data"].append(item)
                     results["errors"][-1]["incidentsNumber"] += 1
@@ -43,3 +36,28 @@ class MessageSort(Data):
             whole_data.append(results)
 
         return whole_data
+
+    @staticmethod
+    def new_block_light(cleared_msg, search_data):
+        block = {
+            "message": cleared_msg,
+            "incidentsNumber": 1,
+            "searchData": search_data
+        }
+        return block
+
+    @staticmethod
+    def new_block_full(cleared_msg, search_data, item):
+        block = MessageSort.new_block_light(cleared_msg, search_data)
+        block["data"] = [item]
+        return block
+
+    @staticmethod
+    def clear_msg(curr_message):
+        return curr_message.replace("\n ", "")
+
+    @staticmethod
+    def search_data(cleared_msg):
+        search_data = re.split(":|=", cleared_msg)
+        search_data.append(cleared_msg)
+        return search_data
